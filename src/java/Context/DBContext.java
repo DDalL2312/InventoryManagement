9c16271ca7329d0a95f61e3ce7414ceb520c9b41
DDalL2312 <datlt2312@gmail.com>
2022-03-22 03:44:48 +0700

    update

--- src/java/Context/DBContext.java
+++ src/java/Context/DBContext.java
@@ -7,21 +7,28 @@ package context;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBContext {
     
     public Connection getConnection() throws Exception {
-        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
+        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName + ";encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
         if (instance == null || instance.trim().isEmpty()) {
             url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
         }
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         return DriverManager.getConnection(url, userID, password);
     }
-    private final String serverName = "KHUONGHUNG";
+    private final String serverName = "dalin.database.windows.net";
     private final String dbName = "Inventory";
     private final String portNumber = "1433";
     private final String instance = "";
-    private final String userID = "sa";
-    private final String password = "142378956";
+    private final String userID = "dalinshop@dalin";
+    private final String password = "tiendat.1423";
+    
+    public static void main(String[] args) throws SQLException, Exception {
+        DBContext db = new DBContext();
+        System.out.println(db.getConnection());
+        System.out.println(db.getConnection().getCatalog());
+    }
 }
